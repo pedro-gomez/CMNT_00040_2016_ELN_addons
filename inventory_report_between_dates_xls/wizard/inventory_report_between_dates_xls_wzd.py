b666bc27223743a7f6f151957598e7e16be4d00c
Pedro Gómez <pegomez@elnogal.com>
2016-10-24 22:45:57 +0200

    Adaptado a stock_move

--- inventory_report_between_dates_xls/wizard/inventory_report_between_dates_xls_wzd.py
+++ inventory_report_between_dates_xls/wizard/inventory_report_between_dates_xls_wzd.py
@@ -25,13 +25,12 @@ from openerp import models, fields, api
 class InventoryReportBetweenDatesXlsWzd(models.TransientModel):
     _name = 'inventory.report.between.dates.xls.wzd'
 
-    date_from = fields.Date('Report Date', required=True, default=fields.Date.today())
-    date_to = fields.Date('Report Date', required=True, default=fields.Date.today())
+    date_from = fields.Date('From Date', required=True, default=fields.Date.today())
+    date_to = fields.Date('To Date', required=True, default=fields.Date.today())
     location_id = fields.Many2one('stock.location', 'Location', required=True)
 
     @api.multi
     def _get_report_data(self):
-        import ipdb; ipdb.set_trace()
         #cr = self.cr
         #uid = self.uid      
         result = []
@@ -39,135 +38,118 @@ class InventoryReportBetweenDatesXlsWzd(models.TransientModel):
         date_start = self.date_from
         date_end = self.date_to
         location_outsource = self.location_id.id
-        sql_dk = '''SELECT product_id,name, code, sum(product_qty_in - product_qty_out) as qty_dk
-                FROM  (SELECT sm.product_id,pt.name , pp.default_code as code,
-                    COALESCE(sum(sm.product_qty),0) AS product_qty_in,
+        sql_dk = '''
+            SELECT product_id, name, code, sum(product_qty_in - product_qty_out) as qty_dk
+                FROM (SELECT sm.product_id, pt.name, pp.default_code as code,
+                    COALESCE(sum(sm.product_qty), 0) AS product_qty_in,
                     0 AS product_qty_out
-                FROM stock_picking sp
-                LEFT JOIN stock_move sm ON sm.picking_id = sp.id
+                FROM stock_move sm
                 LEFT JOIN product_product pp ON sm.product_id = pp.id
                 LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 LEFT JOIN stock_location sl ON sm.location_id = sl.id
-                WHERE date_trunc('day',sm.date) < '%s'
+                WHERE date_trunc('day', sm.date) < '%s'
                 AND sm.state = 'done'
-                --AND sp.location_type = 'outsource_out'
                 AND sm.location_id <> %s
                 AND sm.location_dest_id = %s
-                --AND usage like 'internal'
                 GROUP BY sm.product_id,
-                pt.name ,
+                pt.name,
                 pp.default_code
 
                 UNION ALL
 
-                SELECT sm.product_id,pt.name , pp.default_code as code,
+                SELECT sm.product_id, pt.name, pp.default_code as code,
                     0 AS product_qty_in,
-                    COALESCE(sum(sm.product_qty),0) AS product_qty_out
-
-                FROM stock_picking sp
-                LEFT JOIN stock_move sm ON sm.picking_id = sp.id
+                    COALESCE(sum(sm.product_qty), 0) AS product_qty_out
+                FROM stock_move sm
                 LEFT JOIN product_product pp ON sm.product_id = pp.id
                 LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 LEFT JOIN stock_location sl ON sm.location_id = sl.id
-                WHERE date_trunc('day',sm.date) <'%s'
+                WHERE date_trunc('day', sm.date) < '%s'
                 AND sm.state = 'done'
-                --AND sp.location_type = 'outsource_in'
                 AND sm.location_id = %s
                 AND sm.location_dest_id <> %s
-                --AND usage like 'internal'
                 GROUP BY sm.product_id,
-                pt.name ,
-                pp.default_code) table_dk GROUP BY product_id,name ,code
-                    ''' % (date_start, location_outsource, location_outsource, date_start, location_outsource, location_outsource)
+                pt.name,
+                pp.default_code) table_dk GROUP BY product_id, name, code
+        ''' % (date_start, location_outsource, location_outsource, date_start, location_outsource, location_outsource)
 
         sql_in_tk = '''
-            SELECT sm.product_id,pt.name , pp.default_code as code,
-                    COALESCE(sum(sm.product_qty),0) AS qty_in_tk
-                FROM stock_picking sp
-                LEFT JOIN stock_move sm ON sm.picking_id = sp.id
+            SELECT sm.product_id, pt.name, pp.default_code as code,
+                    COALESCE(sum(sm.product_qty), 0) AS qty_in_tk
+                FROM stock_move sm 
                 LEFT JOIN product_product pp ON sm.product_id = pp.id
                 LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 LEFT JOIN stock_location sl ON sm.location_id = sl.id
-                WHERE date_trunc('day',sm.date) >= '%s'
-                AND date_trunc('day',sm.date) <= '%s'
+                WHERE date_trunc('day', sm.date) >= '%s'
+                AND date_trunc('day', sm.date) <= '%s'
                 AND sm.state = 'done'
-                --AND sp.location_type = 'outsource_out'
                 AND sm.location_id <> %s
                 AND sm.location_dest_id = %s
-                --AND usage like 'internal'
                 GROUP BY sm.product_id,
-                pt.name ,
+                pt.name,
                 pp.default_code
         '''% (date_start, date_end, location_outsource, location_outsource)
 
         sql_out_tk = '''
-            SELECT sm.product_id,pt.name , pp.default_code as code,
-                    COALESCE(sum(sm.product_qty),0) AS qty_out_tk
-                FROM stock_picking sp
-                LEFT JOIN stock_move sm ON sm.picking_id = sp.id
+            SELECT sm.product_id, pt.name, pp.default_code as code,
+                    COALESCE(sum(sm.product_qty), 0) AS qty_out_tk
+                FROM stock_move sm 
                 LEFT JOIN product_product pp ON sm.product_id = pp.id
                 LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 LEFT JOIN stock_location sl ON sm.location_id = sl.id
-                WHERE date_trunc('day',sm.date) >= '%s'
-                AND date_trunc('day',sm.date) <= '%s'
+                WHERE date_trunc('day', sm.date) >= '%s'
+                AND date_trunc('day', sm.date) <= '%s'
                 AND sm.state = 'done'
-                --AND sp.location_type = 'outsource_out'
                 AND sm.location_id = %s
                 AND sm.location_dest_id <> %s
-                --AND usage like 'internal'
                 GROUP BY sm.product_id,
-                pt.name ,
+                pt.name,
                 pp.default_code
         '''% (date_start, date_end, location_outsource, location_outsource)
 
-        sql_ck = '''SELECT product_id,name, code, sum(product_qty_in - product_qty_out) as qty_ck
-                FROM  (SELECT sm.product_id,pt.name , pp.default_code as code,
+        sql_ck = '''
+            SELECT product_id,name, code, sum(product_qty_in - product_qty_out) as qty_ck
+                FROM  (SELECT sm.product_id, pt.name, pp.default_code as code,
                     COALESCE(sum(sm.product_qty),0) AS product_qty_in,
                     0 AS product_qty_out
-                FROM stock_picking sp
-                LEFT JOIN stock_move sm ON sm.picking_id = sp.id
+                FROM stock_move sm 
                 LEFT JOIN product_product pp ON sm.product_id = pp.id
                 LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 LEFT JOIN stock_location sl ON sm.location_id = sl.id
-                WHERE date_trunc('day',sm.date) <= '%s'
+                WHERE date_trunc('day', sm.date) <= '%s'
                 AND sm.state = 'done'
-                --AND sp.location_type = 'outsource_out'
                 AND sm.location_id <> %s
                 AND sm.location_dest_id = %s
-                --AND usage like 'internal'
                 GROUP BY sm.product_id,
-                pt.name ,
+                pt.name,
                 pp.default_code
 
                 UNION ALL
 
-                SELECT sm.product_id,pt.name , pp.default_code as code,
+                SELECT sm.product_id,pt.name, pp.default_code as code,
                     0 AS product_qty_in,
                     COALESCE(sum(sm.product_qty),0) AS product_qty_out
 
-                FROM stock_picking sp
-                LEFT JOIN stock_move sm ON sm.picking_id = sp.id
+                FROM stock_move sm 
                 LEFT JOIN product_product pp ON sm.product_id = pp.id
                 LEFT JOIN product_template pt ON pp.product_tmpl_id = pt.id
                 LEFT JOIN stock_location sl ON sm.location_id = sl.id
-                WHERE date_trunc('day',sm.date) <='%s'
+                WHERE date_trunc('day', sm.date) <= '%s'
                 AND sm.state = 'done'
-                --AND sp.location_type = 'outsource_in'
                 AND sm.location_id = %s
                 AND sm.location_dest_id <> %s
-                --AND usage like 'internal'
                 GROUP BY sm.product_id,
-                pt.name ,
-                pp.default_code) table_ck GROUP BY product_id,name ,code
-                    ''' % (date_end, location_outsource, location_outsource, date_end, location_outsource, location_outsource)
+                pt.name,
+                pp.default_code) table_ck GROUP BY product_id, name, code
+        ''' % (date_end, location_outsource, location_outsource, date_end, location_outsource, location_outsource)
 
         sql = '''
             SELECT ROW_NUMBER() OVER(ORDER BY table_ck.code DESC) AS num,
                     table_ck.product_id, table_ck.name, table_ck.code,
-                    COALESCE(sum(qty_dk),0) as qty_dk,
-                    COALESCE(sum(qty_in_tk),0) as qty_in_tk,
-                    COALESCE(sum(qty_out_tk),0) as qty_out_tk,
-                    COALESCE(sum(qty_ck),0)  as qty_ck
+                    COALESCE(sum(qty_dk), 0) as qty_dk,
+                    COALESCE(sum(qty_in_tk), 0) as qty_in_tk,
+                    COALESCE(sum(qty_out_tk), 0) as qty_out_tk,
+                    COALESCE(sum(qty_ck), 0) as qty_ck
             FROM  (%s) table_ck
                 LEFT JOIN (%s) table_in_tk on table_ck.product_id = table_in_tk.product_id
                 LEFT JOIN (%s) table_out_tk on table_ck.product_id = table_out_tk.product_id
@@ -176,7 +158,6 @@ class InventoryReportBetweenDatesXlsWzd(models.TransientModel):
         ''' %(sql_ck, sql_in_tk, sql_out_tk, sql_dk)
         self.env.cr.execute(sql)
         data = self.env.cr.dictfetchall()
-        import ipdb; ipdb.set_trace()
         for i in data:
             #list_product.append({   'num': i['num'],
             #                        'name': i['name'],
@@ -199,7 +180,6 @@ class InventoryReportBetweenDatesXlsWzd(models.TransientModel):
     @api.multi
     def create_xls_report(self):
         self.ensure_one()
-        import ipdb; ipdb.set_trace()
         res = self._get_report_data()
         data = {}
         #for acc in res:
